04b90b374ebe5a87a1743c9f9adce0ef141ce8e2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-25 14:56:36 +0300

    First App

--- Basic/hello-world/src/App.vue
+++ Basic/hello-world/src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
 import { ref } from 'vue'
 
-// A "ref" is a reactive data source that stores a value.
-// Technically, we don't need to wrap the string with ref()
-// in order to display it, but we will see in the next
-// example why it is needed if we ever intend to change
-// the value.
 const message = ref('Hello World!')
+
+//variable
+const msg = 'Ssup!!'
+
+//functions
+function log() {
+  console.log(msg)
+}
+
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <div @click='log'>{{msg}}</div>
 </template>
 
 <style>
