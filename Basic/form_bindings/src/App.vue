e432ac14a11019b48ce9efdde5e1f66c29dd3ee9
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 10:50:21 +0300

    Form Bindings

---
+++ Basic/form_bindings/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from 'vue'
+
+const header = ref('Shopping list App')
+const items = ref([
+  {id: 1, label: "10 party hats"},
+  {id: 2, label: "2 board games"},
+  {id: 3, label: "20 cups"},
+])
+
+</script>
