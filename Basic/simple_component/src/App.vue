98637412391d01f4fdbad2560e8d2445fdf57e36
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-04 14:21:29 +0300

    TodoItems

--- Basic/simple_component/src/App.vue
+++ Basic/simple_component/src/App.vue
@@ -1,26 +1,19 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<!-- Here we show the simplest possible component which accepts a prop
+and renders it.  -->
+<script setup>
+import { ref } from 'vue'
+import TodoItem from './TodoItem.vue'
+import TodoItem from './TodoItem.vue';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const groceryList = ref([
+  {id: 0, text: 'Vegetables'},
+  {id: 1, text: 'Cheese'},
+  {id: 2, text: 'Whatever else humans are supposed to eat'},
+])
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <ol>
+  <TodoItem />
+  </ol>
+</template>
