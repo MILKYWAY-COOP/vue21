3be69b4ca7f8e251d5420f0a204756c1459f3d72
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-07 18:00:02 +0300

    Temperature Converter

--- 7 GUIs/temperature_converter/src/App.vue
+++ 7 GUIs/temperature_converter/src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
-// const c = ref(0)
-// const f = ref(32) 
+const c = ref(0)
+const f = ref(32)
 
+function setC(e, v = +e.target.value) {
+  console.log(v)
+  c.value = v
+  f.value = v * (9 / 5) + 32
+}
 
+function setF(e, v = +e.target.value) {
+  console.log(v)
+  f.value = v
+  c.value = (v - 32) * (5 / 9)
+}
 </script>
 
 <template>
-  <input/>
-  <input/>
+  <input type="number" :value="c" @change="setC"> Celsius = 
+  <input type="number" :value="f" @change="setF"> Fahrenheit
 </template>
