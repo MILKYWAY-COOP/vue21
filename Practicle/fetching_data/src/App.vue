7c9c14c44d02be418ff3085258a3818db72ed560
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-09 19:10:45 +0300

    API Calls

---
+++ Practicle/fetching_data/src/App.vue
@@ -0,0 +1,10 @@
+<!--
+This example fetches my latest commits data from GitHub’s API and displays them as a list.
+You can switch between the two branches.
+-->
+
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const API_URL = 
+</script>
